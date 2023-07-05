7e93590527dcdf610efc1201cbffa5915bcde0f2
naok1 <puni87gou@gmail.com>
2023-07-05 16:46:04 +0900

    Update example.py

--- li5650/example.py
+++ li5650/example.py
@@ -1,6 +1,7 @@
-from .li5650 import LI5650
+#%%
+from li5650 import LI5650
 import pyvisa as visa
-
+#%%
 if __name__ == "__main__":
     rm=visa.ResourceManager() 
     li5650 = LI5650(rm.open_resource("GPIB0::7::INSTR"))
@@ -15,19 +16,19 @@ if __name__ == "__main__":
     #Reference signal settings
     li5650.reference_source(False, 'RINPut')
     li5650.reference_type(False, 'TPOS')
-    li5650.phase_shift(False, 1, 0)
+    li5650.phase_shift(False, LI5650.Detector.PRIMARY, 0)
     #Primary detector settings
     li5650.set_sensitivity(1,1)
-    li5650.filter_type(False, 1, 'EXPonential')
-    li5650.filter_time_constant(False, 1, 1)
-    li5650.filter_slope(False, 1, 24)
-    li5650.enable_harmonics(False, 1, 'ON', 1)
+    li5650.filter_type(False, LI5650.Detector.PRIMARY, 'EXPonential')
+    li5650.filter_time_constant(False, LI5650.Detector.PRIMARY, 1)
+    li5650.filter_slope(False, LI5650.Detector.PRIMARY, 24)
+    li5650.enable_harmonics(False, LI5650.Detector.PRIMARY, 'ON', 1)
     #Secondary detector settings
     li5650.set_sensitivity(2,200E-6)
-    li5650.filter_type(False, 2, 'EXPonential')
+    li5650.filter_type(False, LI5650.Detector.SECONDARY, 'EXPonential')
     li5650.filter_time_constant(False, 2, 1)
-    li5650.filter_slope(False, 2, 24)
-    li5650.enable_harmonics(False, 2, 'ON', 2)
+    li5650.filter_slope(False, LI5650.Detector.SECONDARY, 24)
+    li5650.enable_harmonics(False, LI5650.Detector.SECONDARY, 'ON', 2)
     li5650.data_transfer_format(False, 'ASCii')
 
     print(li5650.read_data())
