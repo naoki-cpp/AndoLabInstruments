0170f55b3396a606b2df21c4cb81801f4d68f074
naok1 <puni87gou@gmail.com>
2023-07-05 15:22:30 +0900

    added wf1948 example

---
+++ wf1948/example.py
@@ -0,0 +1,16 @@
+from .wf1948 import WF1948
+
+if __name__ == "__main__":
+    wf1948 = WF1948(rm.open_resource("GPIB0::2::INSTR"))
+    wf1948.initialize()
+
+    #Channel 1 settings
+    wf1948.set_frequency(False, 1, 100, 'Hz')
+    wf1948.set_voltage(False, 1, 1.0, "Vpp")
+    wf1948.set_function(False, 1, 'SINusoid')
+    wf1948.output(False, 1, 1)
+    #Channel 2 settings
+    wf1948.set_frequency(False, 2, 200, 'Hz')
+    wf1948.set_voltage(False, 2, 2.0, "Vpp")
+    wf1948.set_function(False, 2, 'SUQare')
+    wf1948.output(False, 2, 1)
