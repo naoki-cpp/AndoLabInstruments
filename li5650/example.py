7dfcde7a4594489add1403232c9de86ac2e32af6
naok1 <puni87gou@gmail.com>
2023-07-07 16:34:41 +0900

    Update example.py

--- li5650/example.py
+++ li5650/example.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
     rm=visa.ResourceManager() 
     li5650 = LI5650(rm.open_resource("GPIB0::7::INSTR"))
     li5650.initialize()
-    li5650.data(False, 31)
+    li5650.data(False, 31) #set read data format. STATUS, DATA1, DATA2, DATA3, DATA4
     #Data Format
-    li5650.set_detect_mode(False, 'DUAL1')
+    li5650.set_detect_mode(False, 'DUAL1') #2-frequency harmonic mode
     li5650.set_data_format('REAL', 'IMAGinary', 'REAL2', 'IMAGinary2')
     #Input settings
     li5650.set_sig('AC', 'GND', 'IE6', 'A')
@@ -16,19 +16,21 @@ if __name__ == "__main__":
     #Reference signal settings
     li5650.reference_source(False, 'RINPut')
     li5650.reference_type(False, 'TPOS')
-    li5650.phase_shift(False, LI5650.Detector.PRIMARY, 0)
     #Primary detector settings
-    li5650.set_sensitivity(1,1)
+    li5650.phase_shift(False, LI5650.Detector.PRIMARY, 0)
+    li5650.set_sensitivity(LI5650.Detector.PRIMARY,1)
     li5650.filter_type(False, LI5650.Detector.PRIMARY, 'EXPonential')
     li5650.filter_time_constant(False, LI5650.Detector.PRIMARY, 1)
     li5650.filter_slope(False, LI5650.Detector.PRIMARY, 24)
     li5650.enable_harmonics(False, LI5650.Detector.PRIMARY, 'ON', 1)
     #Secondary detector settings
-    li5650.set_sensitivity(2,200E-6)
+    li5650.phase_shift(False, LI5650.Detector.SECONDARY, 0)
+    li5650.set_sensitivity(LI5650.Detector.SECONDARY,200E-6)
     li5650.filter_type(False, LI5650.Detector.SECONDARY, 'EXPonential')
-    li5650.filter_time_constant(False, 2, 1)
+    li5650.filter_time_constant(False, LI5650.Detector.SECONDARY, 1)
     li5650.filter_slope(False, LI5650.Detector.SECONDARY, 24)
     li5650.enable_harmonics(False, LI5650.Detector.SECONDARY, 'ON', 2)
+
     li5650.data_transfer_format(False, 'ASCii')
 
     print(li5650.read_data())
