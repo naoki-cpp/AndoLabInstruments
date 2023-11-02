41bacba12635a3bcb2febbe87323bf9fcd5d797d
naok1 <puni87gou@gmail.com>
2023-11-02 14:58:35 +0900

    Create example.py

---
+++ keithley2000/example.py
@@ -0,0 +1,11 @@
+from .keithley2182 import Keithley2182
+import pyvisa as visa
+    
+if __name__ == "__main__":
+    rm=visa.ResourceManager()
+    keithley2000 = Keithley2000(rm.open_resource('GPIB0::14::INSTR'))
+    keithley2000.initialize()
+    keithley2000.configure_DC_volts()
+    keithley2000.configure_trigger(Keithley2000.Source.IMMEDIATE)
+    voltage, _, _ = keithley2000.read_single()
+    print(voltage)
