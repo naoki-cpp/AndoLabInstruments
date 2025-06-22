7ed5d490d929c9d5d0b548ffa53dfd94123e7949
naok1 <puni87gou@gmail.com>
2025-06-22 16:23:00 +0900

    Update example.py

--- keithley2000/example.py
+++ keithley2000/example.py
@@ -12,8 +12,10 @@ if __name__ == "__main__":
     #Thermocouple example
     keithley2000.initialize()
     keithley2000.configure_thermocouple(Keithley2000.THERMOCOUPLE_TYPE.T, Keithley2000.REFERENCE_JUNCTION_TYPE.SIMULATED, 0)
+    # use channel 1 as reference junction which temperature is 0 (ice bath)
     keithley2000.close_single_channel(1)
     keithley2000.configure_temperature(Keithley2000.RESOLUTION._6_5_Digits, Keithley2000.TEMPERATURE_UNITS.CELSIUS, Keithley2000.DIGITAL_FILTER.OFF, Keithley2000.REFERENCE_TYPE.ACQUIRE)
+    # measure the temperature of channel 2
     keithley2000.close_single_channel(2)
     keithley2000.configure_trigger(Keithley2000.Source.IMMEDIATE)
     voltage, unit, channel = keithley2000.read_single(Keithley2000.SOURCE.FUNCTION)
