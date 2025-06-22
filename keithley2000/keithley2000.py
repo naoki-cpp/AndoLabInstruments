005942ab4739416b0f375aaad2d8f49355e74cbd
naok1 <puni87gou@gmail.com>
2025-06-22 16:19:09 +0900

    Update keithley2000.py for TC measurement

--- keithley2000/keithley2000.py
+++ keithley2000/keithley2000.py
@@ -1,25 +1,26 @@
-import pyvisa as visa
+from pymeasure.instruments import Instrument
 from parse import parse
 from enum import Enum
+from pyvisa import constants
 
-class Keithley2000():
-    def __init__(self, instrument:visa.Resource):
-        self.instrument = instrument
-        idn = instrument.query('*IDN?')
-        print(idn)
-        return
-       
-    def __del__(self):
-        self.instrument.close()
-        return
+class Keithley2000(Instrument):
+    def __init__(self, adapter, name="Keithley2000", **kwargs):
+        super().__init__(
+            adapter,
+            name,
+            includeSCPI = False,
+            **kwargs
+        )
     
     def initialize(self):
-        self.instrument.write('*RST')
+        self.write('*RST')
+        self.write('*SRE 48;*ESE 60;*CLS;')
+        self.configure_trigger(self.Source.IMMEDIATE)
         return
     
     def error(self):
-        self.instrument.write(':SYST:ERR?')
-        return self.instrument.read()
+        self.write(':SYST:ERR?')
+        return self.read()
     
     class Digits(Enum):
         _3H     = 4
@@ -41,7 +42,7 @@ class Keithley2000():
         ACQUIRE = 2
 
     def configure_DC_volts(self, resolution:Digits = Digits.DEFAULT, power_line_cycle:float = 1, enable_auto_range:bool = True, custom_range:float = 0, digital_filter:DigitalFilter = DigitalFilter.OFF, digital_filter_readings:int = 10, reference:Reference = Reference.OFF, reference_value:float = 0):
-        self.instrument.write(":FUNC 'VOLT:DC';")
+        self.write(":FUNC 'VOLT:DC';")
 
         err = self.error()
         if(err.split(',')[0] != '0'):print(err)
@@ -50,12 +51,21 @@ class Keithley2000():
             
     
     def abort(self):
-        self.instrument.write(':INIT:CONT OFF')
-        self.instrument.write(':ABOR')
+        self.write(':INIT:CONT OFF')
+        self.write(':ABOR')
         
         err = self.error()
         if(err.split(',')[0] != '0'):print(err)
         
+        return
+    
+    def init_trigger(self, continuous:bool = False):
+        if(continuous):
+            self.write(':INIT:CONT ON;')
+        else:
+            self.write(':INIT:CONT OFF;:INIT;')
+        err = self.error()
+        if(err.split(',')[0] != '0'):print(err)
         return
     
     class Source(Enum):
@@ -69,44 +79,62 @@ class Keithley2000():
         INFINITE    = 0
         CUSTOM      = 1
 
-    def configure_trigger(self, source:Source = Source.IMMEDIATE, timer_delay:float = 0.001, count_mode:CountMode = CountMode.INFINITE, custom_count:int = 1, enable_auto_delay:bool = True, delay:float = 0):
+    def configure_trigger(self, source:Source = Source.IMMEDIATE, timer_delay:float = 0.001, count_mode:CountMode = CountMode.INFINITE, custom_count:int = 1, enable_auto_delay:bool = True, delay:float = 0, init_continuous:bool=True):
 
         self.abort()
 
         if(source == self.Source.IMMEDIATE):
-            self.instrument.write(':TRIG:SOUR IMM')
+            self.write(':TRIG:SOUR IMM')
         if(source == self.Source.TIMER):
-            self.instrument.write(':TRIG:SOUR TIM;TIM ' + str(timer_delay))
+            self.write(':TRIG:SOUR TIM;TIM ' + str(timer_delay))
         if(source == self.Source.MANUAL):
-            self.instrument.write(':TRIG:SOUR MAN')
+            self.write(':TRIG:SOUR MAN')
         if(source == self.Source.BUS):
-            self.instrument.write(':TRIG:SOUR BUS')
+            self.write(':TRIG:SOUR BUS')
         if(source == self.Source.EXTERNAL):
-            self.instrument.write(':TRIG:SOUR EXT')
+            self.write(':TRIG:SOUR EXT')
         
         if(count_mode == self.CountMode.INFINITE):
-            self.instrument.write(':TRIG:COUN INF')
+            self.write(':TRIG:COUN INF')
         if(count_mode == self.CountMode.CUSTOM):
-            self.instrument.write(':TRIG:COUN ' + str(custom_count))
+            self.write(':TRIG:COUN ' + str(custom_count))
         
         if(enable_auto_delay):
-            self.instrument.write(':TRIG:DEL:AUTO ON')
+            self.write(':TRIG:DEL:AUTO ON')
         else:
-            self.instrument.write(':TRIG:DEL:AUTO OFF')
-            self.instrument.write(':TRIG:DEL ' + str(delay))
-        
+            self.write(':TRIG:DEL:AUTO OFF')
+            self.write(':TRIG:DEL ' + str(delay))
+        self.init_trigger(init_continuous)
         err = self.error()
         if(err.split(',')[0] != '0'):print(err)
         
         return
-    def initiate_measurement(self):
-        self.instrument.write(':FORM ASC;:FORM:ELEM READ,UNIT,CHAN;')
-        self.instrument.write('INITiate')
+    def send_trigger(self):
+        self.write('*TRG')
+        err = self.error()
+        if(err.split(',')[0] != '0'):print(err)
         return
+    
+    class SOURCE(Enum):
+        FUNCTION    =   0
+        CALC1       =   1
+        CALC2       =   2
+        CALC3       =   3
 
-    def read_single(self):
-        self.instrument.write(':DATA:FRESh?')
-        data = self.instrument.read().strip()
+    def read_single(self, source:SOURCE):
+        self.write(':FORM ASC')
+        self.write(':FORM:ELEM READ,UNIT,CHAN')
+        self.wait_for_srq('*SRE 1;:STAT:MEAS:ENAB 32;*CLS;')
+        match source:
+            case self.SOURCE.FUNCTION:
+                self.write(':SENS:DATA?')
+            case self.SOURCE.CALC1:
+                self.write(':CALC1:DATA?')
+            case self.SOURCE.CALC2:
+                self.write(':CALC2:DATA?')
+            case self.SOURCE.CALC3:
+                self.write(':CALC3:LIM:FAIL?')
+        data = self.read().strip()
         raw, channel = data.split(',')
         raw_voltage, strings = parse("{:f}{:S}", raw)
         voltage = float(str(raw_voltage) + strings[0:4])
@@ -116,3 +144,102 @@ class Keithley2000():
         if(err.split(',')[0] != '0'):print(err)
         
         return voltage, unit, channel
+    def wait_for_srq(self, command, timeout=25000):
+        # Type of event we want to be notified about
+        event_type = constants.EventType.service_request
+        # Mechanism by which we want to be notified
+        event_mech = constants.EventMechanism.queue
+        self.adapter.connection.enable_event(event_type, event_mech)
+        self.adapter.connection.discard_events(event_type, event_mech)
+        self.write(command)
+        # Wait for the event to occur
+        self.adapter.connection.wait_for_srq(timeout)
+        self.adapter.connection.enable_event(event_type, event_mech)
+        self.adapter.connection.discard_events(event_type, event_mech)
+        self.write('*SRE 48;')
+        return
+################################################
+### Thermocouple settings
+################################################
+    class THERMOCOUPLE_TYPE(Enum):
+            J   =   'J'
+            K   =   'K'
+            T   =   'T'
+    
+    class REFERENCE_JUNCTION_TYPE(Enum):
+        SIMULATED   =   0
+        REAL        =   1
+
+    def configure_thermocouple(self, thermocouple_type:THERMOCOUPLE_TYPE, reference_junction_type:REFERENCE_JUNCTION_TYPE, simulated_temperature=23, temperature_coefficient=2E-4, voltage_offset=5.463E-2):
+        self.write(':TEMP:TC:TYPE ' + thermocouple_type.value)
+        match reference_junction_type:
+            case self.REFERENCE_JUNCTION_TYPE.SIMULATED:
+                self.write(':TEMP:TC:RJUN:RSEL SIM')
+                self.write(':TEMP:TC:RJUN:SIM ' + str(simulated_temperature))
+            case self.REFERENCE_JUNCTION_TYPE.REAL:
+                self.write(':TEMP:TC:RJUN:RSEL REAL')
+                self.write(':TEMP:TC:RJUN:REAL:TCO ' + str(temperature_coefficient))
+                self.write(':TEMP:TC:RJUN:REAL:OFFSET ' + str(voltage_offset))
+        return
+    
+    class RESOLUTION(Enum):
+        _3_5_Digits = 4
+        _4_5_Digits = 5
+        _5_5_Digits = 6
+        _6_5_Digits = 7
+
+    class TEMPERATURE_UNITS(Enum):
+        CELSIUS     =   'C'
+        FAHREHEIT   =   'F'
+        KELVIN      =   'K'
+    
+    class DIGITAL_FILTER(Enum):
+        OFF                 =   0
+        MOVING_FILTER       =   1
+        REPEATING_FILTER    =   2
+    
+    class REFERENCE_TYPE(Enum):
+        OFF     =   0
+        SET     =   1
+        ACQUIRE =   2
+
+    def configure_temperature(self, resolution:RESOLUTION, temperature_units:TEMPERATURE_UNITS, digital_filter_type:DIGITAL_FILTER, reference_type:REFERENCE_TYPE, reference_value=0, power_line_cycles=1.00, digital_filter_readings=10):
+        self.write(":FUNC 'TEMP'")
+        self.write(':TEMP:DIG ' + str(resolution.value))
+        self.write(':UNIT:TEMP ' + temperature_units.value)
+        self.write(':TEMP:NPLC ' + str(power_line_cycles))
+        match digital_filter_type:
+            case self.DIGITAL_FILTER.OFF:
+                self.write(':TEMP:AVER:STAT OFF')
+            case self.DIGITAL_FILTER.MOVING_FILTER:
+                self.write(':TEMP:AVER:TCON MOV')
+                self.write('COUN ' + str(digital_filter_readings))
+                self.write('STAT ON')
+            case self.DIGITAL_FILTER.REPEATING_FILTER:
+                self.write(':TEMP:AVER:TCON REP')
+                self.write('COUN ' + str(digital_filter_readings))
+                self.write('STAT ON')
+        match reference_type:
+            case self.REFERENCE_TYPE.OFF:
+                self.write(':TEMP:REF:STAT OFF')
+            case self.REFERENCE_TYPE.SET:
+                self.write(':TEMP:REF ' + str(reference_value))
+                self.write('REF:STAT ON')
+            case self.REFERENCE_TYPE.ACQUIRE:
+                self.wait_for_srq('*SRE 1;:STAT:MEAS:ENAB 32;*CLS;')
+                self.write(':TEMP:REF:STAT ON;:TEMP:REF:ACQ')
+        err = self.error()
+        if(err.split(',')[0] != '0'):print(err)
+        return
+
+################################################
+### TCscan settings
+################################################
+    def open_all_channel(self):
+        self.write(':ROUTe:OPEN:ALL')
+        return
+    
+    def close_single_channel(self, channel:int):
+        self.write(':ROUTE:CLOSe (@%d)' % channel)
+        return
+    
