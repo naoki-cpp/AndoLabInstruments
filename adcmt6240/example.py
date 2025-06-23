d0205dc6f7a57ab6b6c8e75288ebbf6488cdc0d8
naok1 <puni87gou@gmail.com>
2025-06-23 19:37:40 +0900

    Update example.py

--- adcmt6240/example.py
+++ adcmt6240/example.py
@@ -4,7 +4,7 @@ import pyvisa as visa
 #%%
 if __name__ == "__main__":
     rm=visa.ResourceManager() 
-    adcmt6240 = ADCMT6240(rm.open_resource('GPIB0::5::INSTR'))
+    adcmt6240 = ADCMT6240('GPIB0::5::INSTR')
     adcmt6240.initialize()
     adcmt6240.configure_output(ADCMT6240.SourceFunctionType.VOLTAGE, ADCMT6240.OutputModeType.DC)
     adcmt6240.configure_limiter(ADCMT6240.SourceFunctionType.VOLTAGE, 0.03) #limit 30mA
