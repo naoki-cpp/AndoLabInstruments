768a28e26c046f543e683f7cb02bc54ebff99cdb
naok1 <puni87gou@gmail.com>
2025-06-23 19:55:40 +0900

    Update keithley2182.py

--- keithley2182/keithley2182.py
+++ keithley2182/keithley2182.py
@@ -1,19 +1,17 @@
-import pyvisa as visa
+from pymeasure.instruments import Instrument
 from enum import Enum
 
-class Keithley2182:
-    def __init__(self, instrument:visa.Resource):
-        self.instrument = instrument
-        idn = instrument.query('*IDN?')
-        print(idn)
-        return
-       
-    def __del__(self):
-        self.instrument.close()
-        return
+class Keithley2182(Instrument):
+    def __init__(self, adapter, name="Keithley2182", **kwargs):
+        super().__init__(
+            adapter,
+            name,
+            includeSCPI = False,
+            **kwargs
+        )
     
     def initialize(self):
-        self.instrument.write('*RST;')
+        self.write('*RST;')
         return
     
     class Channel(Enum):
@@ -25,24 +23,24 @@ class Keithley2182:
     def configure_analog_filter(self, channel:Channel, enable:bool):
         if(channel == self.Channel.VOLTAGE_CHANNEL_1):
             if(enable):
-                self.instrument.write(':SENSe:VOLTage:LPASs ON;')
+                self.write(':SENSe:VOLTage:LPASs ON;')
             else:
-                self.instrument.write(':SENSe:VOLTage:LPASs OFF;')
+                self.write(':SENSe:VOLTage:LPASs OFF;')
         if(channel == self.Channel.VOLTAGE_CHANNEL_2):
             if(enable):
-                self.instrument.write(':SENSe:VOLTage:CHANnel2:LPASs ON;')
+                self.write(':SENSe:VOLTage:CHANnel2:LPASs ON;')
             else:
-                self.instrument.write(':SENSe:VOLTage:CHANnel2:LPASs OFF;')
+                self.write(':SENSe:VOLTage:CHANnel2:LPASs OFF;')
         if(channel == self.Channel.TEMPERATURE_CHANNEL_1):
             if(enable):
-                self.instrument.write(':SENSe:TEMPerature:LPASs ON;')
+                self.write(':SENSe:TEMPerature:LPASs ON;')
             else:
-                self.instrument.write(':SENSe:TEMPerature:LPASs OFF;')
+                self.write(':SENSe:TEMPerature:LPASs OFF;')
         if(channel == self.Channel.TEMPERATURE_CHANNEL_2):
             if(enable):
-                self.instrument.write(':SENSe:TEMPerature:CHANnel2:LPASs ON;')
+                self.write(':SENSe:TEMPerature:CHANnel2:LPASs ON;')
             else:
-                self.instrument.write(':SENSe:TEMPerature:CHANnel2:LPASs OFF;')
+                self.write(':SENSe:TEMPerature:CHANnel2:LPASs OFF;')
         return
 
     class RateUnit(Enum):
@@ -51,33 +49,33 @@ class Keithley2182:
     
     def configure_measurement(self, channel:Channel, integration_rate:int, integration_rate_units:RateUnit, sample_count:int):
         if(channel == self.Channel.VOLTAGE_CHANNEL_1):
-            self.instrument.write(":SENSe:FUNCtion 'VOLT';:SENSe:CHANnel 1;")
+            self.write(":SENSe:FUNCtion 'VOLT';:SENSe:CHANnel 1;")
             if(integration_rate_units == self.RateUnit.LINE_CYCLES):
-                self.instrument.write(":SENSe:VOLTage:NPLCycles " + str(integration_rate))
+                self.write(":SENSe:VOLTage:NPLCycles " + str(integration_rate))
             if(integration_rate_units == self.RateUnit.SECONDS):
-                self.instrument.write(":SENSe:VOLTage:APERture " + str(integration_rate))
+                self.write(":SENSe:VOLTage:APERture " + str(integration_rate))
 
         if(channel == self.Channel.VOLTAGE_CHANNEL_2):
-            self.instrument.write(":SENSe:FUNCtion 'VOLT';:SENSe:CHANnel 1;")
+            self.write(":SENSe:FUNCtion 'VOLT';:SENSe:CHANnel 1;")
             if(integration_rate_units == self.RateUnit.LINE_CYCLES):
-                self.instrument.write(":SENSe:VOLTage:NPLCycles " + str(integration_rate))
+                self.write(":SENSe:VOLTage:NPLCycles " + str(integration_rate))
             if(integration_rate_units == self.RateUnit.SECONDS):
-                self.instrument.write(":SENSe:VOLTage:APERture " + str(integration_rate))
+                self.write(":SENSe:VOLTage:APERture " + str(integration_rate))
 
         if(channel == self.Channel.TEMPERATURE_CHANNEL_1):
-            self.instrument.write(":SENSe:FUNCtion 'TEMP';:SENSe:CHANnel 1;")
+            self.write(":SENSe:FUNCtion 'TEMP';:SENSe:CHANnel 1;")
             if(integration_rate_units == self.RateUnit.LINE_CYCLES):
-                self.instrument.write(":SENSe:TEMPerature:NPLCycles " + str(integration_rate))
+                self.write(":SENSe:TEMPerature:NPLCycles " + str(integration_rate))
             if(integration_rate_units == self.RateUnit.SECONDS):
-                self.instrument.write(":SENSe:TEMPerature:APERture " + str(integration_rate))
+                self.write(":SENSe:TEMPerature:APERture " + str(integration_rate))
 
         if(channel == self.Channel.TEMPERATURE_CHANNEL_2):
-            self.instrument.write(":SENSe:FUNCtion 'TEMP';:SENSe:CHANnel 2;")
+            self.write(":SENSe:FUNCtion 'TEMP';:SENSe:CHANnel 2;")
             if(integration_rate_units == self.RateUnit.LINE_CYCLES):
-                self.instrument.write(":SENSe:TEMPerature:NPLCycles " + str(integration_rate))
+                self.write(":SENSe:TEMPerature:NPLCycles " + str(integration_rate))
             if(integration_rate_units == self.RateUnit.SECONDS):
-                self.instrument.write(":SENSe:TEMPerature:APERture " + str(integration_rate))    
-        self.instrument.write(':SAMPle:COUNt ' + str(sample_count))
+                self.write(":SENSe:TEMPerature:APERture " + str(integration_rate))    
+        self.write(':SAMPle:COUNt ' + str(sample_count))
         return
     
     class InitiateType(Enum):
@@ -86,11 +84,11 @@ class Keithley2182:
 
     def initiate_measurement(self, initiate_type:InitiateType):
         if(initiate_type == self.InitiateType.IMMIDIATE):
-            self.instrument.write(':INIT:CONT OFF;:INIT;')
+            self.write(':INIT:CONT OFF;:INIT;')
         if(initiate_type == self.InitiateType.CONTINUOUS):
-            self.instrument.write(':INIT:CONT ON')
+            self.write(':INIT:CONT ON')
         return
     
     def fetch(self):
-        self.instrument.write(':SENSe:DATA:FRESh?')
-        return float(str.strip(self.instrument.read()))
+        self.write(':SENSe:DATA:FRESh?')
+        return float(str.strip(self.read()))
