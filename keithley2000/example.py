edae2d9f87d55fd31dbd9ce22c2156ae8651f7c2
naok1 <puni87gou@gmail.com>
2025-12-09 12:13:33 +0900

    Refactor DC voltage and thermocouple measurements
    Updated thermocouple configuration and measurement settings.

--- keithley2000/example.py
+++ keithley2000/example.py
@@ -1,24 +1,20 @@
 from keithley2000 import Keithley2000
+
 if __name__ == "__main__":
     keithley2000 = Keithley2000('GPIB0::14::INSTR')
-    #DC Voltage measurement
     keithley2000.initialize()
     keithley2000.configure_DC_volts()
-    keithley2000.configure_trigger(Keithley2000.Source.IMMEDIATE)
-    voltage, unit, channel = keithley2000.read_single(Keithley2000.SOURCE.FUNCTION)
+    keithley2000.configure_trigger(Keithley2000.TriggerSource.IMMEDIATE)
+    keithley2000.initiate_measurement()
+    voltage, _, _ = keithley2000.read_single()
     print(voltage)
-    print(unit)
-    print(channel)
-    #Thermocouple example
+    #Thermocouple
     keithley2000.initialize()
-    keithley2000.configure_thermocouple(Keithley2000.THERMOCOUPLE_TYPE.T, Keithley2000.REFERENCE_JUNCTION_TYPE.SIMULATED, 0)
-    # use channel 1 as reference junction which temperature is 0 (ice bath)
-    keithley2000.close_single_channel(1)
-    keithley2000.configure_temperature(Keithley2000.RESOLUTION._6_5_Digits, Keithley2000.TEMPERATURE_UNITS.CELSIUS, Keithley2000.DIGITAL_FILTER.OFF, Keithley2000.REFERENCE_TYPE.ACQUIRE)
-    # measure the temperature of channel 2
-    keithley2000.close_single_channel(2)
-    keithley2000.configure_trigger(Keithley2000.Source.IMMEDIATE)
-    voltage, unit, channel = keithley2000.read_single(Keithley2000.SOURCE.FUNCTION)
+    keithley2000.configure_thermocouple(Keithley2000.THERMOCOUPLE_TYPE.T, Keithley2000.REFERENCE_JUNCTION_TYPE.SIMULATED, 30)
+    keithley2000.configure_temperature(Keithley2000.RESOLUTION._6_5_Digits,Keithley2000.TEMPERATURE_UNITS.KELVIN,Keithley2000.DIGITAL_FILTER.OFF,Keithley2000.REFERENCE_TYPE.OFF)
+    keithley2000.initiate_measurement()
+    voltage, unit, channel = keithley2000.read_single()
     print(voltage)
     print(unit)
     print(channel)
+    
