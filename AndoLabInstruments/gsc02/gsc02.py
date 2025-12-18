fcf98487a29bdf7fa96a3dc9febbadd0bd45b2ee
naok1 <puni87gou@gmail.com>
2025-12-18 21:53:47 +0900

    Update gsc02.py

--- AndoLabInstruments/gsc02/gsc02.py
+++ AndoLabInstruments/gsc02/gsc02.py
@@ -1,21 +1,21 @@
-import pyvisa as visa
+from pymeasure.instruments import Instrument
 from enum import Enum
 
-class GSC02():
-    def __init__(self, instrument:visa.Resource):
-        self.instrument = instrument
-        return
-       
-    def __del__(self):
-        self.instrument.close()
-        return
+class GSC02(Instrument):
+    def __init__(self, adapter, name="GSC02", **kwargs):
+        super().__init__(
+            adapter,
+            name,
+            includeSCPI = False,
+            **kwargs
+        )
     
     class SpeedRange(Enum):
         LOW_SPEED   = 1
         HIGH_SPEED  = 2
     
     def set_speed(self, speed_range:SpeedRange, low_speed1:int, high_speed1:int, acceleration_time1:int, low_speed2:int, high_speed2:int, acceleration_time2:int):
-        self.instrument.write('D:{:1d}S{:d}F{:d}R{:d}S{:d}F{:d}R{:d}'.format(speed_range.value, low_speed1, high_speed1, acceleration_time1, low_speed2, high_speed2, acceleration_time2))
+        self.write('D:{:1d}S{:d}F{:d}R{:d}S{:d}F{:d}R{:d}'.format(speed_range.value, low_speed1, high_speed1, acceleration_time1, low_speed2, high_speed2, acceleration_time2))
         return
     
     class Axis(Enum):
@@ -27,5 +27,5 @@ class GSC02():
         BACKWARD    = '-'
 
     def move(self, axis:Axis, direction:Direction, pulse_num:int):
-        self.instrument.write('M:' + axis.value + direction.value + 'P' + str(pulse_num))
-        self.instrument.write('G')
+        self.write('M:' + axis.value + direction.value + 'P' + str(pulse_num))
+        self.write('G')
