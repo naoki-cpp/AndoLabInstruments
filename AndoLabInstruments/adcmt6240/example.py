e54c93db89010c48c96b8d08e46e06258d4ec2be
naok1 <puni87gou@gmail.com>
2025-12-18 21:34:32 +0900

    Clean up code

--- AndoLabInstruments/adcmt6240/example.py
+++ AndoLabInstruments/adcmt6240/example.py
@@ -1,9 +1,6 @@
 #%%
 from adcmt6240 import ADCMT6240
-import pyvisa as visa
-#%%
 if __name__ == "__main__":
-    rm=visa.ResourceManager() 
     adcmt6240 = ADCMT6240('GPIB0::5::INSTR')
     adcmt6240.initialize()
     adcmt6240.configure_output(ADCMT6240.SourceFunctionType.VOLTAGE, ADCMT6240.OutputModeType.DC)
@@ -12,5 +9,3 @@ if __name__ == "__main__":
     adcmt6240.configure_trigger(ADCMT6240.TriggerMode.HOLD)  # trigger mode hold
     adcmt6240.configure_measurement(ADCMT6240.MeasurementType.CURRENT)  # current measurement
     adcmt6240.operate_output() # output on
-
-# %%
