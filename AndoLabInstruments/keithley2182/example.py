561a962b4c58ffa40e13280272d872854010d2f3
naok1 <puni87gou@gmail.com>
2025-12-18 21:22:52 +0900

    Clean up example.py by removing unused code
    Removed unused import and resource manager initialization.

--- AndoLabInstruments/keithley2182/example.py
+++ AndoLabInstruments/keithley2182/example.py
@@ -1,14 +1,9 @@
-#%%
 from .keithley2182 import Keithley2182
-import pyvisa as visa
     
 if __name__ == "__main__":
-    rm=visa.ResourceManager()
     keithley2182 = Keithley2182('GPIB0::5::INSTR')
     keithley2182.initialize()
     keithley2182.configure_analog_filter(Keithley2182.Channel.VOLTAGE_CHANNEL_1, False)
     keithley2182.configure_measurement(Keithley2182.Channel.VOLTAGE_CHANNEL_1, 1, Keithley2182.RateUnit.LINE_CYCLES, 1)
     keithley2182.initiate_measurement(Keithley2182.InitiateType.IMMIDIATE)
     print(keithley2182.fetch())
-
-# %%
